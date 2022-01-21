c312fc39ee89940080abc48510ec201f876af52a
Iandi Santulus <iandisantulusn@gmail.com>
2022-01-21 15:45:37 +0800

    fix(todo): add module imports

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoResolver } from './todo.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './entities/todo.entity';
+import { TodoDetail } from './entities/todo-detail.entity';
+import { User } from 'src/user/entities/user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  providers: [TodoService, TodoResolver]
+  imports: [AuthModule, TypeOrmModule.forFeature([Todo, TodoDetail, User])],
+  providers: [TodoService, TodoResolver],
+  exports: [TodoService],
 })
 export class TodoModule {}
