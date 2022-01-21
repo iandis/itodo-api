5acba240b49533cbb6aad2e7e9cf05553735c711
Iandi Santulus <iandisantulusn@gmail.com>
2022-01-21 15:43:19 +0800

    chore(auth-guard): change to using underscore

--- src/auth/app-auth.guard.ts
+++ src/auth/app-auth.guard.ts
@@ -11,12 +11,12 @@ export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 @Injectable()
 export class AppAuthGuard extends NestAuthGuard('jwt') {
-  constructor(private reflector: Reflector, private authService: AuthService) {
+  constructor(private readonly _reflector: Reflector, private readonly _authService: AuthService) {
     super();
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+    const isPublic = this._reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
@@ -27,7 +27,7 @@ export class AppAuthGuard extends NestAuthGuard('jwt') {
     if (!ctx?.headers?.authorization) {
       throw new AuthenticationError('You must provide token.');
     }
-    ctx.userId = await this.authService.verify(ctx.headers.authorization);
+    ctx.userId = await this._authService.verify(ctx.headers.authorization);
     return !!ctx.userId;
   }
 }
