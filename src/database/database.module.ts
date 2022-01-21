95ec05beb81cc08f14b393e78fb4e143a2e25ea4
Iandi Santulus <iandisantulusn@gmail.com>
2022-01-21 12:56:33 +0800

    feat(database): init database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from 'src/config/config.service';
+import { TodoDetail } from 'src/todo/entities/todo-detail.entity';
+import { Todo } from 'src/todo/entities/todo.entity';
+import { User } from 'src/user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          name: 'default',
+          type: 'postgres',
+          host: config.DB_HOST,
+          port: config.DB_PORT,
+          username: config.DB_USERNAME,
+          password: config.DB_PASSWORD,
+          database: config.DB_DATABASE,
+          keepConnectionAlive: true,
+          entities: [Todo, TodoDetail, User],
+          synchronize: false,
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
