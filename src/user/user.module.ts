0aaa6a568628e36d363664c6ad8618a8984abf17
Iandi Santulus <iandisantulusn@gmail.com>
2022-01-21 15:46:09 +0800

    fix(user): fix module imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import { User } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule, TypeOrmModule.forFeature([User])],
   providers: [UserService, UserResolver],
+  exports: [UserService],
 })
 export class UserModule {}
