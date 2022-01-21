8bdc054b7765119afe870da018174ccbb0d92e4c
Iandi Santulus <iandisantulusn@gmail.com>
2022-01-22 00:10:15 +0800

    fix(user): fix user input validation

--- src/user/dto/request/user-update.input.ts
+++ src/user/dto/request/user-update.input.ts
@@ -1,22 +1,26 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsOptional, MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class UserUpdateInput {
+  @IsOptional()
   @MinLength(1)
   @MaxLength(50)
   @Field(() => String, { nullable: true })
   name?: string;
 
-  @MinLength(5)
+  @IsOptional()
+  @IsEmail()
   @MaxLength(100)
   @Field(() => String, { nullable: true })
   email?: string;
 
+  @IsOptional()
   @MaxLength(150)
   @Field(() => String, { nullable: true })
   image?: string;
 
+  @IsOptional()
   @MaxLength(250)
   @Field(() => String, { nullable: true })
   about?: string;
