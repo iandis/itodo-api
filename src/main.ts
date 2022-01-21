bef42c109530479c4a8cc1b6155193ff5799b1a7
Iandi Santulus <iandisantulusn@gmail.com>
2022-01-21 15:47:27 +0800

    fix(main): add listen to port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as firebaseAdmin from 'firebase-admin';
 import { applicationContext } from './app.context';
 import * as serviceAccount from 'credentials/firebase-admin-service-account.json';
 import { ServiceAccount } from 'firebase-admin';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap(): Promise<void> {
   const app = await applicationContext();
@@ -10,5 +11,8 @@ async function bootstrap(): Promise<void> {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount as ServiceAccount),
   });
+  const config = app.get(ConfigService);
+  await app.listen(config.PORT);
+  Logger.log(`Server running on http://localhost:${config.PORT}/graphql`, '[itodo-api]');
 }
-bootstrap().catch((err) => Logger.error(err.message, err.stack, '[main]'));
+bootstrap().catch((err) => Logger.error(err.message, err.stack, '[itodo-api]'));
