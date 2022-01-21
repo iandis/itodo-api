9cbad0828c104ea7377088ad21edf0d9ddb649ad
Iandi Santulus <iandisantulusn@gmail.com>
2022-01-21 15:47:10 +0800

    fix(app): fix module imports
    add graphql module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,33 @@ import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLError, GraphQLFormattedError } from 'graphql/error';
+import { graphqlErrorHandler } from './shared/helpers/common/graphql-error-handler.helper';
+import { APP_GUARD } from '@nestjs/core';
+import { AppAuthGuard } from './auth/app-auth.guard';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, ConfigModule, TodoModule, UserModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    ConfigModule,
+    TodoModule,
+    UserModule,
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: false,
+      autoSchemaFile: 'schema.gql',
+      context: ({ req, res }) => ({ req, res, headers: req.headers }),
+      formatError: (err: GraphQLError): GraphQLFormattedError => {
+        return graphqlErrorHandler(err);
+      },
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AppAuthGuard,
+    }
+  ]
 })
 export class AppModule {}
